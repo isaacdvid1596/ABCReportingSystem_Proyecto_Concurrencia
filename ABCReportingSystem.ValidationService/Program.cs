c70987488e9720fefeb83e919ed5cd8fce48a11b
Julio Isaac David Ramos <isaacdvid15.96@outlook.com>
2021-09-07 21:21:27 -0600

    added new project

---
+++ ABCReportingSystem.ValidationService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ABCReportingSystem.RecollectorService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var rootPath = @"C:\Users\isaac\Documents\files";
+
+            foreach (string file in Directory.EnumerateFiles(rootPath,"*.csv"))
+            {
+                
+            }
+        }
+    }
+}
