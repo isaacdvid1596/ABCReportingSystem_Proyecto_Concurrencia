75d1836056e8fae022a7e20d7162197a800494f4
Julio Isaac David Ramos <isaacdvid15.96@outlook.com>
2021-09-08 19:46:26 -0600

    added csv helper reader to recollector service

--- ABCReportingSystem.ValidationService/Program.cs
+++ ABCReportingSystem.ValidationService/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace ABCReportingSystem.RecollectorService
 {
@@ -8,12 +13,34 @@ namespace ABCReportingSystem.RecollectorService
         static void Main(string[] args)
         {
 
-            var rootPath = @"C:\Users\isaac\Documents\files";
+            var records = new List<Sales>();
 
-            foreach (string file in Directory.EnumerateFiles(rootPath,"*.csv"))
+            using (var streamReader = new StreamReader(@"C:\Users\isaac\Documents\files\sales.csv"))
             {
                 
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    csvReader.Context.RegisterClassMap<SalesClassMap>();
+                    records = csvReader.GetRecords<Sales>().ToList();
+                }
             }
         }
     }
+
+    public class SalesClassMap : ClassMap<Sales>
+    {
+        public SalesClassMap()
+        {
+            Map(m => m.AutomobileId).Name("car_id");
+            Map(m => m.BuyerId).Name("buyer_id");
+            Map(m => m.DivisionId).Name("division_id");
+            Map(m => m.FirstName).Name("buyer_first_name");
+            Map(m => m.LastName).Name("buyer_last_name");
+            Map(m => m.Price).Name("price");
+            Map(m => m.Username).Name("username");
+            Map(m => m.Vin).Name("vin");
+        }
+    }
+
+
 }
---
+++ ABCReportingSystem.ValidationService/Sales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCReportingSystem.RecollectorService
+{
+    public class Sales
+    {
+        public string Username { get; set; }
+
+        public Guid AutomobileId { get; set; }
+
+        public double Price { get; set; }
+
+        public Guid Vin { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Guid BuyerId { get; set; }
+
+        public int DivisionId { get; set; }
+    }
+}
