a46440fa3d4a30fc77f6c5d12fc7840aa4298e06
diegodru <diegodramos@gmail.com>
2021-09-20 17:58:29 -0600

    :v

--- ABCReportingSystem.ValidationService/Program.cs
+++ ABCReportingSystem.ValidationService/Program.cs
@@ -6,58 +6,74 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using CsvHelper;
 using CsvHelper.Configuration;
-using RabbiMQ.Client;
+using RabbitMQ.Client;
 
 namespace ABCReportingSystem.RecollectorService
 {
   public class Program {
       static void Main(string[] args) {
-        Console.log("hola");
 
         var factory = new ConnectionFactory() { HostName = "localhost" };
         var records = new List<Sales>();
 
-        using (var streamReader = new StreamReader(@"/home/diego/School/Unitec/Periodo8/Concu/proyecto/sales.csv")) {
+        using (var streamReader = new StreamReader(@"/home/diego/School/Unitec/Periodo8/Concu/proyecto/sales2.csv")) {
             using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture)) {
               csvReader.Context.RegisterClassMap<SalesClassMap>();
               records = csvReader.GetRecords<Sales>().ToList();
               Sales[] sales = records.ToArray();
               try{
                 using (var connection = factory.CreateConnection()) {
-                  int count = records.Count();
-                  Task[] tasks = new Task[count / 50 + 1];
-                  for(int i = 0; i < tasks.Length; i++){
-                    tasks[i] = Task.Factory.StartNew(() => {
-                          int max = (i < count / 50) ? 50 : count % 50;
-                          for(int j = i * 50; j < i * 50 + max; j++){
-                            using (var channel = connection.CreateModel()) {
-                              channel.QueueDeclare(queue: i.ToString(),
+                  var _channel = connection.CreateModel();
+                  _channel.QueueDeclare(queue: "init",
                                 durable: false,
                                 exclusive: false,
                                 autoDelete: false,
                                 arguments: null);
-                              string registro = sales[j].Username + ',' +
-                                sales[i].AutomobileId + ',' + 
-                                sales[i].Price + ',' + 
-                                sales[i].Vin.ToString() +  ',' +
-                                sales[i].FirstName + ',' + 
-                                sales[i].LastName + ',' + 
-                                sales[i].BuyerId.ToString() + ',' + 
-                                sales[i].DivisionId.ToString() ;
-                              var body = Encoding.UTF8.GetBytes(registro);
-                              Console.Write($"thread{i}");
+
+                  int count = records.Count;
+                  Task[] tasks = new Task[count / 50 + 1];
+                  var body = Encoding.UTF8.GetBytes((count / 50 + 1).ToString());
+                  _channel.BasicPublish(exchange: "",
+                    routingKey: "init",
+                    basicProperties: null,
+                    body: body);
+                  for(int i = 0; i < tasks.Length; i++){
+                    int current = i;
+                    tasks[i] = Task.Factory.StartNew(() => {
+                          int max = (current < count / 50) ? 50 : count % 50;
+                          for(int j = current * 50; j < current * 50 + max; j++){
+                            using (var channel = connection.CreateModel()) {
+                              channel.QueueDeclare(queue: current.ToString(),
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+                              string registro_json = JsonSerializer.Serialize(sales[i]);
+                              //string registro = sales[j].Username + ',' +
+                                //sales[j].AutomobileId + ',' + 
+                                //sales[j].Price + ',' + 
+                                //sales[j].Vin.ToString() +  ',' +
+                                //sales[j].FirstName + ',' + 
+                                //sales[j].LastName + ',' + 
+                                //sales[j].BuyerId.ToString() + ',' + 
+                                //sales[j].DivisionId.ToString() ;
+                              var body = Encoding.UTF8.GetBytes(registro_json);
 															channel.BasicPublish(exchange: "",
-                                routingKey: i.ToString(),
+                                routingKey: current.ToString(),
                                 basicProperties: null,
                                 body: body);
                             }
                           }
                         });
                     }
+                  Task.WaitAll(tasks);
                 }
               }catch(Exception e) {
+                Console.WriteLine(e.ToString());
               }
             }
         }
---
+++ ValService/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CsvHelper;
+using CsvHelper.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ValService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        var records = new List<Sales>();
+
+				using(var connection = factory.CreateConnection())
+        using(var _channel = connection.CreateModel())
+        {
+            _channel.QueueDeclare(queue: "init",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            int threads = 1;
+            var _consumer = new EventingBasicConsumer(_channel);
+            _consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                threads = Int32.Parse(message);
+                //Console.WriteLine(" [x] Received {0}", threads);
+                Task[] tasks = new Task[threads];
+                for(int i = 0; i < threads; i++){
+                  int current = i;
+                  tasks[i] = Task.Factory.StartNew(() => {
+                      using(var channel = connection.CreateModel()){
+                        channel.QueueDeclare(queue: current.ToString(),
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+                      var consumer = new EventingBasicConsumer(channel);
+                      consumer.Received += (model, ea) =>
+                      {
+                          var body = ea.Body.ToArray();
+                          var registro_json = Encoding.UTF8.GetString(body);
+                          Console.WriteLine($"thread: {current}: {registro_json}");
+                          //Console.WriteLine(" [x] Received {0}", registro_json);
+                      };
+                      channel.BasicConsume(queue: current.ToString(),
+                                           autoAck: true,
+                                           consumer: consumer);
+
+                      }
+                      });
+                }
+            };
+            _channel.BasicConsume(queue: "init",
+                                 autoAck: true,
+                                 consumer: _consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        //using (var streamReader = new StreamReader(@"/home/diego/School/Unitec/Periodo8/Concu/proyecto/sales2.csv")) {
+            //using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture)) {
+              //csvReader.Context.RegisterClassMap<SalesClassMap>();
+              //records = csvReader.GetRecords<Sales>().ToList();
+              //Sales[] sales = records.ToArray();
+            //}
+        //}
+        }
+    }
+}
